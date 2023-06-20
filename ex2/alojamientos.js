a201e4f7a24022b8ed4c44080d76766f28f69fbb
Nicolás Miguel Ayut González <nayugon@canariaseducacion.es>
2023-06-20 23:08:33 +0100

    servicio

--- ex2/alojamientos.js
+++
@@ -1,3 +0,0 @@
-fetch("https://services.arcgis.com/hkQNLKNeDVYBjvFE/arcgis/rest/services/Alojamientos_turisticos/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&outSR=4326&f=json")
-.then(res=>res.json())
-.then(data=>console.log(data))
