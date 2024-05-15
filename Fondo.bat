f01224396ea0d2a4a1fa06359d72dbf5f69d30f3
nayugon <115787206+nayugon@users.noreply.github.com>
2024-05-15 16:44:23 +0100

    Add files via upload
    Añadido vbs para poner en Inicio que llama al bat que ejecuta el proyecto fondo

---
+++ Fondo.bat
@@ -0,0 +1,5 @@
+::Copiar en C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup
+@echo OFF
+cd C:\DPTOINFJMPP\Wallpaper\
+node .\CreaFondo.js
+
---
+++ fondo.vbs
@@ -0,0 +1,2 @@
+set objshell = createobject("wscript.shell")
+objshell.run "C:\DPTOINFJMPP\Wallpaper\fondo.bat", vbhide
